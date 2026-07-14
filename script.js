5a2211ed0476469f63177e9f5c51abfc149c1717
Vivek Upadhyay <upadhayayvivek5777@gmail.com>
2026-07-14 20:04:49 +0530

    Update project

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-function getWeather() {
+const  getWeather=async()=> {
     const city = document.getElementById("city").value;
 
     if(city === ""){
         alert("Please enter a city name");
         return;
     }
-
-    document.getElementById("weatherInfo").innerHTML = `
+fetch(`https://wttr.in/${city}?format=j1`)
+  .then(res => res.json()
+)
+  .then(data =>{ console.log(data)
+        document.getElementById("weatherInfo").innerHTML = `
         <h2>${city}</h2>
-        <div class="temp">${Math.floor(Math.random()*15)+20}°C</div>
+        <div class="temp">${data.current_condition[0].temp_C}°C</div>
         <p>🌤 Sunny</p>
         <div class="details">
             <span>💧 60%</span>
             <span>🌬 10 km/h</span>
         </div>
     `;
+  });
 }
